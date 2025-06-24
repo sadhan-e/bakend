007ee3b3b4b4f9f05ada19adcd4acf47237be38d
raut-rahul-2137 <142197737+raut-rahul-2137@users.noreply.github.com>
2025-06-25 00:08:41 +0530

    j

--- backend/settings.py
+++ backend/settings.py
@@ -29,14 +29,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-a-default-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['https://frontend-sigma-five-55.vercel.app', 'https://bakend-1-avw8.onrender.com']
+ALLOWED_HOSTS = ['https://bakend-1-avw8.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Allow cloudflare tunnels
-ALLOWED_HOSTS.append('.trycloudflare.com')
+
 
 
 # Application definition
